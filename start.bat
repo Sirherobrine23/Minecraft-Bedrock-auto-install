a12a092579033cab4aec4a12539e45e307b97480
Matheus sampaio Queiroga <50121801+Sirherobrine23@users.noreply.github.com>
2020-05-16 17:04:01 -0300

    install sucess

--- start.bat
+++ start.bat
@@ -1,13 +1,20 @@
-echo off
+@echo off
 
-set URL_INSTALL=https://minecraft.azureedge.net/bin-win/bedrock-server-1.14.60.5.zip
-set PATH_TO_INSTALL=%HOMEPATH%\mcpe
-set WIN_DIR_C=C:/Users/Mathe/mcpe/
+rem variaveis
+set URL_INSTALL=https://minecraft.azureedge.net/bin-win/bedrock-server-1.14.60.5.zip >>install.log 2>&1
+set PATH_TO_INSTALL=%HOMEPATH%\mcpe >>install.log 2>&1
+set WIN_DIR_C=C:/Users/Mathe/mcpe/ >>install.log 2>&1
 
 
-mkdir %WIN_DIR_C% ;
-mkdir mcpe
-wget.exe --no-check-certificate %URL_INSTALL% -O mcpe.zip ;
-unzip mcpe.zip -d mcpe/
+echo "Baixando os arquivos e fazendo os prerequisitos"
+rm -rf mcpe >>install.log 2>&1
+mkdir %WIN_DIR_C% >>install.log 2>&1
+mkdir mcpe >>install.log 2>&1
+wget.exe --no-check-certificate %URL_INSTALL% -O mcpe.zip >>install.log 2>&1
+unzip mcpe.zip -d mcpe/ >>install.log 2>&1
+rm -rf mcpe/whitelist.json >>install.log 2>&1
+rm -rf mcpe/server.properties >>install.log 2>&1
+wget.exe --no-check-certificate https://raw.githubusercontent.com/Sirherobrine23/Minecraft-Bedrock-auto-install/Beta/server.properties -O mcpe/server.properties >>install.log 2>&1
+wget.exe --no-check-certificate https://raw.githubusercontent.com/Sirherobrine23/Minecraft-Bedrock-auto-install/Beta/whitelist.json -O mcpe/whitelist.json >>install.log 2>&1
 
-pause
+rem cp -r mcpe/* %PATH_TO_INSTALL%
