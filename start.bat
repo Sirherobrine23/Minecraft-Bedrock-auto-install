21da602c986b522bfa15f9fcd55fabcb1835c96f
Matheus sampaio Queiroga <50121801+Sirherobrine23@users.noreply.github.com>
2020-05-16 15:06:57 -0300

    +~-

--- start.bat
+++ start.bat
@@ -1,8 +1,10 @@
-set URL_INSTALL=https://minecraft.azureedge.net/bin-linux/bedrock-server-1.14.60.5.zip
+echo off
+
+set URL_INSTALL=https://minecraft.azureedge.net/bin-win/bedrock-server-1.14.60.5.zip
 set PATH_TO_INSTALL=%HOMEPATH%\mcpe
 set WIN_DIR_C=C:/Users/Mathe/mcpe/mcpe.zip
 
 
-mkdir %PATH_TO_INSTALL%
-wget.exe %URL_INSTALL% -O %WIN_DIR_C%/mcpe/mcpe.zip
-unzip -o "%PATH_TO_INSTALL%\mcpe\mcpe.zip" -d "%PATH_TO_INSTALL%\mcpe"
+mkdir %WIN_DIR_C% ;
+wget.exe --no-check-certificate %URL_INSTALL% -O mcpe.zip ;
+unzip -o mcpe.zip -d %WIN_DIR_C%/mcpe/ ;
