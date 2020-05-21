628a7bb02f32a49f82f55235f886b740eae6d6f5
Matheus sampaio Queiroga <50121801+Sirherobrine23@users.noreply.github.com>
2020-05-21 19:10:37 -0300

    fix

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ echo "instalando o git e o curl";
 sudo Apt install git -y;
 echo "Baixando o Instalador";
 git clone https://github.com/Sirherobrine23/Minecraft-Bedrock-auto-install.git installer
-sudo chmod a+x installer/menu;
+sudo chmod a+x installer/menu.sh;
 echo "pronto. execute os comandos abaixo";
-echo '"cd installer"';
-echo 'ou "./menu -h"';
+echo '"cd installer", "./menu.sh"';
