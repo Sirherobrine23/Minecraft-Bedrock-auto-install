1300a60020dbada0cfedbd9e526e2094fb69703e
Matheus sampaio Queiroga <50121801+Sirherobrine23@users.noreply.github.com>
2020-05-20 21:33:31 -0300

    update

--- mcpe-start.sh
+++ mcpe-start.sh
@@ -2,5 +2,7 @@
 cd DIRE/mcpe/
 LD_LIBRARY_PATH=. ./bedrock_server
 echo 'Fazendo backup do mapa'
-cd worlds/ ; zip $(TZ=UTC+3 date +"%d-%m-%Y-%H-%M-%S")-"MAPASS".zip -r "MAPASS" 
+cd worlds/ ; zip $(TZ=UTC+3 date +"%d-%m-%Y-%H-%M-%S")-"MAPASS".zip -r "MAPASS"
+desc=$(TZ=UTC+3 date +"%d-%m-%Y-%H-%M-%S")
+cd backup ; git add . ; git commit -m "$desc" ; git push
 exit 1
