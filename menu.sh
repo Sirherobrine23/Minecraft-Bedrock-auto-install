fc81924bd60f58aa51fc57081494fc41d0acf478
Matheus sampaio Queiroga <50121801+Sirherobrine23@users.noreply.github.com>
2020-05-24 22:34:50 -0300

    echo has been changed but has not yet been fully translated (half mouth translation)

--- menu.sh
+++ menu.sh
@@ -29,22 +29,22 @@ sudo mkdir $TMP >>$USUARIO/log.txt 2>&1 ;
 sudo rm -rf $TMP/level.txt >>$USUARIO/log.txt 2>&1 ;
 
 #Espaço
-echo "o Diretorio do seu usuario é $USUARIO"
-echo "Começamos ás $(TZ=UTC+3 date +"%H:%M:%S")"
+echo "your user's directory is $USUARIO"
+echo "We start at $(TZ=UTC+3 date +"%H:%M:%S")"
 echo " "
 echo "--------------"
 echo " "
 
 #Root
 if [ "$EUID" -ne 0 ]; then
-	echo "Você não está executando o script com root ou sudo"
+	echo "You are not running the root or sudo script"
 	exit 1
 fi
 if [[ -e /etc/debian_version ]]; then
 	source /etc/os-release
 	OS=$ID # debian or ubuntu
 else
-	echo "Você não tem instalado ou não esta com sistema Debian ou Ubuntu "
+	echo "You do not have or are not running Debian or Ubuntu"
 	exit 1
 fi
 case $1 in
@@ -54,19 +54,19 @@ case $1 in
                   cat banner.txt;
                   # Prerequisite
                   echo "  ";
-                  echo "Instalando os Pré-reuisitos para o  ubuntu";
+                  echo "Installing Pre-Requests for Ubuntu";
                   echo -ne "#                         (01%)\r";
                   sudo apt install -y wget unzip >>$USUARIO/log.txt 2>&1 ;
                   echo -ne "##                        (02%)\r";
                   echo " ";
-                  echo "Criando diretorio do servidor no $PATH_TO_INSTALL"
+                  echo "Creating server directory at $ PATH_TO_INSTALL $PATH_TO_INSTALL"
                   sudo mkdir $PATH_TO_INSTALL >>$USUARIO/log.txt 2>&1 ;
                   #Download do arquivos servidor
                   echo -ne "#####                     (20%)\r";
-                  echo "Baixando o Software do Servidor";
+                  echo "Downloading the Server Software";
                   sudo wget $BDS -O $PATH_TO_INSTALL/mcpe.zip >>$USUARIO/log.txt 2>&1 ;
                   echo -ne "########                  (40%)\r";
-                  echo "Instalando o Servidor";
+                  echo "Installing the Server";
                   sudo unzip -o $PATH_TO_INSTALL/mcpe.zip -d $PATH_TO_INSTALL/mcpe >>$USUARIO/log.txt 2>&1 ;
                   sudo rm -rf $PATH_TO_INSTALL/mcpe.zip;
                   echo -ne "#############             (50%)\r";
@@ -76,22 +76,22 @@ case $1 in
                   cp -r ./server.properties $PATH_TO_INSTALL/mcpe/ >>$USUARIO/log.txt 2>&1 ;
                   cp -r ./whitelist.json $PATH_TO_INSTALL/mcpe/ >>$USUARIO/log.txt 2>&1 ;
                   echo -ne "#######################   (100%)\r";
-                  echo -ne "####### completo ######   (100%)\r";
-                  echo "O log está no arquivo $USUARIO/log.txt ou /tmp/install.log"
+                  echo -ne "####### complete ######   (100%)\r";
+                  echo "The log is in the file $USUARIO/log.txt or/tmp/install.log"
       elif [[ $OS == 'debian' ]]; then
                   #banner
                   cat banner.txt;
                   # Prerequisite
                   echo "  ";
-                  echo "Instalando os Pré-reuisitos para o debian";
+                  echo "Installing the pre-requirements for debian";
                   echo -ne "#                         (01%)\r";
                   sudo apt install -y wget unzip >>$USUARIO/log.txt 2>&1 ;
                   echo -ne "##                        (02%)\r";
                   echo " ";
-                  echo "Criando o Diretorio do servidor no $PATH_TO_INSTALL";
+                  echo "Creating the server directory in $PATH_TO_INSTALL";
                   sudo mkdir $PATH_TO_INSTALL >>$USUARIO/log.txt 2>&1 ;
                   #Download do arquivos servidor
-                  echo "Baixando o Software do Servidor";
+                  echo "Downloading the Server Software";
                   sudo wget $BDS -O $PATH_TO_INSTALL/mcpe.zip >>$USUARIO/log.txt 2>&1 ;
                   echo -ne "########                  (40%)\r";
                   sudo unzip -o $PATH_TO_INSTALL/mcpe.zip -d $PATH_TO_INSTALL/mcpe >>$USUARIO/log.txt 2>&1 ;
@@ -103,8 +103,8 @@ case $1 in
                   cp -r ./server.properties $PATH_TO_INSTALL/mcpe/ >>$USUARIO/log.txt 2>&1 ;
                   cp -r ./whitelist.json $PATH_TO_INSTALL/mcpe/ >>$USUARIO/log.txt 2>&1 ;
                   echo -ne "#######################   (100%)\r";
-                  echo -ne "####### completo ######   (100%)\r";
-                  echo "O log está no arquivo $USUARIO/log.txt ou /tmp/install.log"
+                  echo -ne "####### complete ######   (100%)\r";
+                  echo "The log is in the file $USUARIO/log.txt or /tmp/install.log"
       fi
       ;;
       "--update")
@@ -117,7 +117,7 @@ case $1 in
                         #banner
                         cat banner.txt;
                         #Copiando
-                        echo -ne "Atualizando versão do Ubuntu\r"
+                        echo -ne "Updating Ubuntu version\r"
                         sleep 2
                         echo -ne "Começado\r"
                         echo -ne "                          (0%)\r";
@@ -153,14 +153,14 @@ case $1 in
                         sudo rm -rf $PATH_TO_INSTALL/mcpe/worlds/whitelist.json >>$USUARIO/log.txt 2>&1 ;
                         sudo rm -r $PATH_TO_BACKUP
                         echo -ne "#######################   (100%)\r";
-                        echo -ne "###### Completo #######   (100%)\r";
-                        echo "O log está no arquivo $USUARIO/log.txt ou /tmp/install.log"
+                        echo -ne "###### complete #######   (100%)\r";
+                        echo "The log is in the file $USUARIO/log.txt or /tmp/install.log"
             elif [[ $OS == 'debian' ]]; then
                         #banner
                         cat banner.txt;
-                        echo -ne "Atualizando versão do Debian\r"
+                        echo -ne "Upgrading Debian version\r"
                         sleep 2
-                        echo -ne "Começado\r"
+                        echo -ne "Started\r"
                         #Copiando
                         echo -ne "                          (0%)\r";
                         sudo mkdir $PATH_TO_BACKUP >>$USUARIO/log.txt 2>&1 ;
@@ -195,17 +195,9 @@ case $1 in
                         sudo rm -rf $PATH_TO_INSTALL/mcpe/worlds/whitelist.json >>$USUARIO/log.txt 2>&1 ;
                         sudo rm -r $PATH_TO_BACKUP
                         echo -ne "#######################   (100%)\r";
-                        echo -ne "###### Completo #######   (100%)\r";
-                        echo "O log está no arquivo $USUARIO/log.txt ou /tmp/install.log"
+                        echo -ne "###### complete #######   (100%)\r";
+                        echo "The log is in the file $USUARIO/log.txt or /tmp/install.log"
             fi
-      ;;
-      "--screen") 
-            #install
-            echo "Instalando o Screen ";
-            sudo apt install -y screen ;
-            sudo bash menu --ubuntu;
-            sudo mv mcpe /sbin/ ; sudo chmod a+x /sbin/mcpe;
-            echo "DICA: Para deixar o servidor em segundo plano aperte CRTL + A + D. deixara em segundo plano para voltar basta executar o comando screen -r"
       ;;
       "--start-file")
             #Config File
@@ -222,20 +214,20 @@ case $1 in
       ;;
       "--sistema")
 	if [[ -e /sbin/mcpe ]]; then
-            echo "Percebi que já tem o --start-file pronto. Otimo!"
-            echo "montando o arquivos para que tudo nos ajude"
+            echo "I noticed that you already have the --start-file ready. Great!"
+            echo "assembling the files so that everything helps us"
             sudo cp start-on-system.sh /etc/init.d/mcpe;
-            echo "copiando o arquivo";
+            echo "copying the file";
             sudo chmod a+x /etc/init.d/mcpe;
             update-rc.d mcpe defaults
-            echo "pronto ele inicia junto com sistema(Beta), o comando abaixo pode ajudar"
+            echo "ready it starts together with system (Beta), the command below can help"
             echo " "
             echo 'sudo service mcpe start | stop | restart'
             echo " "
 	else
             #Config File
-            echo "percebir que não executou --start-file, sem problema vamos fazer isso agora!"
-            echo "montando o arquivo necessario espere"
+            echo "realize you didn't run --start-file, no problem let's do it now!"
+            echo "mounting the necessary file wait"
             rm -rf /tmp/level.txt
             rm -rf /sbin/mcpe
             cat $FILE2 > "$file"
@@ -244,15 +236,15 @@ case $1 in
             sed -i "s|MAPASS|$(cat /tmp/level.txt)|g" "$file"
             sed -i "s|PATH_TO_INSTALL|$PATH_TO_INSTALL|g" "$file"
             sudo mv "$file" /sbin/mcpe ; sudo chmod a+x /sbin/mcpe ;
-            echo "arquivo pronto"
-            echo "agora o outro arquivo"
+            echo "file ready"
+            echo "now the other file"
             #pos
             sudo rm -rf /etc/init.d/mcpe
             sudo cp start-on-system.sh /etc/init.d/mcpe;
-            echo "copiando o arquivo";
+            echo "copying the file";
             sudo chmod a+x /etc/init.d/mcpe;
             update-rc.d mcpe defaults
-            echo "pronto ele inicia junto com sistema(Beta) o comando abaixo pode ajudar"
+            echo "ready it starts together with system (Beta) the command below can help"
             echo 'sudo service mcpe start | stop | restart'
             echo " "
 	fi
@@ -265,10 +257,10 @@ case $1 in
             IP_V6=$(ifconfig $SEARCH_IPV6 | grep "inet6" | awk -F' ' '{print $2}' | awk '{print $1}') 
             IP_PUBLICO=$(wget -qO- http://ipecho.net/plain)
             #Echo's
-                  echo "Seu IPv4 é $IP_V4 para Jogar localmente , e o Seu IPv6 é $(echo -ne $IP_V6)"
+                  echo "Your IPv4 is $IP_V4 to Play locally, and Your IPv6 is $(echo -ne $ IP_V6)"
                   echo " "
-                  echo "Caso Queira Jogar Remotamento com alguém Abra as Portas 19132 e 19133 no seu Roteador ou seu Firewall, seu IPv4 Publico é $IP_PUBLICO"
-                  echo "Também verifique se sua operadora ou provedor libera as portas do servidor. contate-os"
+                  echo "If you want to play Remoting with someone Open Ports 19132 and 19133 on your Router or Firewall, your IPv4 Public is $IP_PUBLICO"
+                  echo "Also check if your operator or provider releases the server's ports. contact them"
             echo " ";
       ;;
       "--unistall") sudo rm -rf $REMOVE
@@ -281,6 +273,6 @@ esac
 echo " "
 echo "--------------"
 echo " "
-echo "qualquer erro no script me comunique no https://github.com/Sirherobrine23/Minecraft-Bedrock-auto-install/issues"
-echo "Terminamos as $(TZ=UTC+3 date +"%H:%M:%S")"
+echo "any error in the script please let me know at https://github.com/Sirherobrine23/Minecraft-Bedrock-auto-install/issues"
+echo "We finished the $(TZ=UTC+3 date +"%H:%M:%S")"
 sudo rm -rf mcpe.zip
