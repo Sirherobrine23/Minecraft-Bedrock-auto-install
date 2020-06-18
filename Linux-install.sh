55e4078ab05e0adb637e35303c42561dfac51269
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-06-18 00:17:02 -0300

    Update Linux-install.sh

--- Linux-install.sh
+++ Linux-install.sh
@@ -3,7 +3,7 @@
 echo "instalando o git e o curl";
 sudo Apt install git dos2unix -y;
 echo "Baixando o Instalador";
-git clone https://github.com/Sirherobrine23/Minecraft-Bedrock-auto-install.git installer
+git clone https://github.com/Sirherobrine23/Minecraft-Bedrock-auto-install.git -b linux installer
 sudo chmod a+x installer/menu.sh;
 cd installer
 dos2unix */*/*/*/*/*/*
